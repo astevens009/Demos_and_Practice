47a04086d2da1552f7d25d3952edad0f3ad1b480
Andre Stevens <astevens009@gmail.com>
2023-10-31 20:31:19 -0400

    Crafting ideas for unit testing

---
+++ StarfleetShips/README.md
@@ -0,0 +1,4 @@
+# Ideas:
+1. Instantiate the class inside the module. This will require assigning any objects instantiated outside of the module to the inner instantiated object
+2. Create getters/setters inside the module to access the variables in the module
+3. Create global variable in the module that can access the local variables?
--- StarfleetShips/ships_inventory.py
+++ StarfleetShips/ships_inventory.py
@@ -1,5 +1,24 @@
+import logging
+from enum import Enum, auto
+
 class FleetVessel:
-    def __init__(self, registration, name, status):
+    """
+    Starfleet ship object
+    @param registration : ship's registration ID
+    @param name : ship's name
+    @param statis : the current status of the ship
+    """
+    
+    class Status(Enum):
+        Commissioned = auto()
+        Active = auto()
+        Decommissioned = auto()
+        Repairs = auto()
+        On_Assignment = auto()
+        Destroyed = auto()
+        Missing = auto()
+    
+    def __init__(self, registration = None, name = None, status = Status.Commissioned):
         self.ship_registration = registration
         self.ship_name = name
         self.current_status = status
@@ -7,10 +26,40 @@ class FleetVessel:
     def __str__(self):
         return(f"\nShip Information:\n{self.ship_name}\nRegistration ID: {self.ship_registration}\nCurrent Status: {self.current_status}\n")
     
+# region Instantiating the Class
+    """
+    Instantiating the class so that we can access its members. To be defined at a later time.
+    """
+    
+    # fleet_ship = FleetVessel()
+# endregion
+    
 def get_ship_name(ship : FleetVessel):
     return ship.self.ship_name
 
+def get_ship_registration(ship: FleetVessel):
+    return ship.self.ship_registration
 
+def get_ship_status(ship : FleetVessel):
+    return ship.self.current_status
+
+def set_ship_name(ship : FleetVessel):
+    if ship.ship_name == None or ship.ship_name == "":
+        logging.warn("The ship has no name. The ship instance doesn't exist yet.")
+    else:
+        ship.self.ship_name = ship.ship_name
+
+def set_ship_registration(ship: FleetVessel):
+    if ship.ship_registrtion == None or ship.ship_registration == "":
+        logging.warn("The ship has no registration. The ship instance doesn't exist yet.")
+    else:
+        ship.self.ship_registration = ship.ship_registration
+
+def set_ship_status(ship : FleetVessel):
+    if ship.current_status == None or ship.current_status == "":
+        logging.warn("The ship has no current status. The ship instance doesn't exist yet.")
+    else:
+        ship.self.current_status = ship.current_status
     
 
 # if __name__ == '__main__':
